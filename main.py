c30920cf73fd3f44c79599f8e7371102e9b5ad90
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2022-12-18 12:51:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ t1, t2 = st.columns((0.10,1))
 
 t1.image(image, width = 180)
 original_title = '<p style="font-family:Courier; color:"#228B22"; font-size: 35px;">MedTech Hospital Experience Dashboard - MedMIB Hospital</p>'
-t2.title(original_title, unsafe_allow_html=True)
+t2.markdown(original_title, unsafe_allow_html=True)
 t2.markdown("### This section shows some information about MeHEDI - Tool of Patient Satisfaction")
 
 df = connect_to_gsheet()
