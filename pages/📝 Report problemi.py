bace38677ef07df76d3a906f5d4478e3305f954e
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2022-12-27 11:11:25 +0100

    Update 📝 Report problemi.py

--- pages/📝 Report problemi.py
+++ pages/📝 Report problemi.py
@@ -2,3 +2,41 @@ import streamlit as st
 
 st.title("Report problemi")
 st.markdown("Prova")
+
+#gsheet_connector = connect_to_gsheet()
+
+st.sidebar.write(
+    f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data."
+)
+
+st.sidebar.write(
+    f"[Read more](https://docs.streamlit.io/knowledge-base/tutorials/databases/public-gsheet) about connecting your Streamlit app to Google Sheets."
+)
+
+form = st.form(key="annotation")
+
+with form:
+    cols = st.columns((1, 1))
+    author = cols[0].text_input("Report author:")
+    bug_type = cols[1].selectbox(
+        "Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2
+    )
+    comment = st.text_area("Comment:")
+    cols = st.columns(2)
+    date = cols[0].date_input("Bug date occurrence:")
+    bug_severity = cols[1].slider("Bug severity:", 1, 5, 2)
+    submitted = st.form_submit_button(label="Submit")
+
+
+if submitted:
+    add_row_to_gsheet(
+        gsheet_connector,
+        [[author, bug_type, comment, str(date), bug_severity]],
+    )
+    st.success("Thanks! Your bug was recorded.")
+    st.balloons()
+
+expander = st.expander("See all records")
+with expander:
+    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+    st.dataframe(get_data(gsheet_connector))
