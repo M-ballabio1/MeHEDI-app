d5cda2c4863d363026706aa736809d92ae7d43fc
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2022-12-27 11:43:47 +0100

    Update 📊 Sezione Personale Sanitario.py

--- pages/📊 Sezione Personale Sanitario.py
+++ pages/📊 Sezione Personale Sanitario.py
@@ -1,3 +1,32 @@
-import streamlit as st
+import pickle
+from pathlib import Path
+
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
 
 st.title("Sezione Personale Sanitario")
