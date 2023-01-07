83c0970ca0ccc86ffd5edfe6c236358ec5fae1ba
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-07 14:18:22 +0100

    Update Patient_Form.py

--- Patient_Form.py
+++ Patient_Form.py
@@ -94,7 +94,116 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
     "sales_dashboard", "abcdef", cookie_expiry_days=30)
 name, authentication_status, username = authenticator.login("Login - Web application Customer Satisfaction CX", "main")
 
-page_bg_img = f"""
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+    st.write('<base target="_blank">', unsafe_allow_html=True)
+    prev_time = [time.time()]
+    a, b = st.columns([1, 10])
+    with a:
+        st.image(image, width=175)      
+    with b:
+        st.text("")  
+
+if authentication_status:
+    #placeholder.empty()
+
+    # ---- SIDEBAR ----
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+    
+    def form_pazienti():
+        t1, t2 = st.columns((1, 0.15)) 
+
+        t1.title("MedTech Hospital Experience Dashboard - MedMIB Hospital")
+        t1.markdown("### This section shows some information about MeHEDI - Tool of Patient Satisfaction")
+        t2.image(image, width = 150)
+
+        #append pages
+        #st.sidebar.success("Select a page above.")
+
+        # connect and append data
+        df = connect_to_gsheet()
+        #expander = st.expander("See all records")
+        #with expander:
+        #    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+        #    st.dataframe(get_data(df))
+
+        with st.expander("ℹ️ Istruzioni generali", expanded=False):
+            st.markdown(
+                """
+                ## Informazioni Tool MeHEDI
+                L'obiettivo di questo tool è quello di fornire uno strumento per una misurazione
+                della soddsifazione dei pazienti
+                """
+            )
+            st.markdown("")
+            st.markdown(
+                """
+                ## Perchè misurare Patients Satisfaction?
+                L'efficacia di un'iniziativa di business non è data solo dalla misurazione degli outcome (es. revenue),
+                1. Miglioramento percepito della Customer Experience dei prodotti e servizi creati e lanciati rispetto al passato
+                2. Miglioramento dell'Employee Experience portata dal nuovo framework operativo
+                3. Miglioramento della performance di business (Outcome) - revenue, costi ecc
+                """
+            )
+            st.markdown("")
+            #st.image(image)
+        st.write("")
+
+
+        # creazione webform
+
+        form = st.form(key="annotation", clear_on_submit = True,)
+        with form:
+            cols = st.columns((1, 1, 1))
+
+            #info paziente
+            author = cols[0].text_input("Nome del paziente:")
+            eta = cols[1].text_input("Età anagrafica:")
+            sesso = cols[2].selectbox(
+                "Sesso:", ["Maschio", "Femmina", "Non specificato"], index=2)
+            #email = cols[3].text_input("Età anagrafica:")
+
+            cols2 = st.columns((4))
+            #infrastruttura fisica tecnologica
+            date = cols2[0].date_input("Quando è stato in ospedale:")
+            infras = cols2[1].slider("Qualità della struttura ospedaliera :", 1, 100, 1)
+            proces = cols2[2].slider("Qualità dei processi clinici amministrativi :", 1, 100, 1)
+            sicurezza = cols2[3].slider("Sicurezza ospedale :", 1, 100, 1)
+
+            cols3 = st.columns((1, 1, 1, 1))
+            #risorse umane e accoglienza
+            qualita = cols3[0].slider("Qualità del personale :", 1, 100, 1)
+            pulizia = cols3[1].slider("Pulizia degli ambienti :", 1, 100, 1)
+            empatia = cols3[2].slider("Grado di empatia personale :", 1, 100, 1)
+            info_terapeutiche = cols3[3].slider("Chiarezze delle informazioni terapeutiche :", 1, 100, 1)
+            submitted = st.form_submit_button(label="Submit")
+            if submitted==True:
+                datetime_object = datetime.datetime.now()
+                st.success("Successfully")
+                add_row_to_gsheet(
+                df, [[author, eta, sesso,
+                      str(date), infras, proces, sicurezza,
+                      qualita, pulizia, empatia, info_terapeutiche, str(datetime_object)]])
+                st.balloons()
+                
+    if name=="Matteo Ballabio":
+        page_names_to_funcs = {
+            "Form Patient Satisfaction": form_pazienti,
+            "Bug Report": bug_fun,
+            "Dashboard": dashboard_fun}
+    elif name=="Antonio Di Pasquale":
+        page_names_to_funcs = {
+            "Form Customer Satisfaction": form_pazienti}
+
+    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys(), key ="value")
+    page_names_to_funcs[selected_page]()
+
+    st.sidebar.title("About")
+    page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
     background-image: url("https://www.xmple.com/wallpaper/gradient-linear-orange-grey-1920x1080-c2-dcdcdc-ff8c00-a-180-f-14.svg");
@@ -117,99 +226,20 @@ page_bg_img = f"""
     }}
     </style>
     """
-st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.sidebar.info(
+        """
+        Questa è una webapp creata da Pwc Italy che consente di valutare Customer Satisfaction dei tool
+        offerti al cliente Rossi.Spa.
+        
+        Web App URL: <https://xxx.streamlitapp.com>
+        """
+    )
 
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-    st.write('<base target="_blank">', unsafe_allow_html=True)
-    prev_time = [time.time()]
-    a, b = st.columns([1, 10])
-    with a:
-        st.image(image, width=175)      
-    with b:
-        st.text("")  
-
-if authentication_status:
-    #placeholder.empty()
-
-    # ---- SIDEBAR ----
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title(f"Welcome {name}")
-
-    t1, t2 = st.columns((1, 0.15)) 
-
-    t1.title("MedTech Hospital Experience Dashboard - MedMIB Hospital")
-    t1.markdown("### This section shows some information about MeHEDI - Tool of Patient Satisfaction")
-    t2.image(image, width = 150)
-
-    #append pages
-    #st.sidebar.success("Select a page above.")
-
-    # connect and append data
-    df = connect_to_gsheet()
-    #expander = st.expander("See all records")
-    #with expander:
-    #    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-    #    st.dataframe(get_data(df))
-
-    with st.expander("ℹ️ Istruzioni generali", expanded=False):
-        st.markdown(
-            """
-            ## Informazioni Tool MeHEDI
-            L'obiettivo di questo tool è quello di fornire uno strumento per una misurazione
-            della soddsifazione dei pazienti
-            """
-        )
-        st.markdown("")
-        st.markdown(
-            """
-            ## Perchè misurare Patients Satisfaction?
-            L'efficacia di un'iniziativa di business non è data solo dalla misurazione degli outcome (es. revenue),
-            1. Miglioramento percepito della Customer Experience dei prodotti e servizi creati e lanciati rispetto al passato
-            2. Miglioramento dell'Employee Experience portata dal nuovo framework operativo
-            3. Miglioramento della performance di business (Outcome) - revenue, costi ecc
-            """
-        )
-        st.markdown("")
-        #st.image(image)
-    st.write("")
-
-
-    # creazione webform
-
-    form = st.form(key="annotation", clear_on_submit = True,)
-    with form:
-        cols = st.columns((1, 1, 1))
-
-        #info paziente
-        author = cols[0].text_input("Nome del paziente:")
-        eta = cols[1].text_input("Età anagrafica:")
-        sesso = cols[2].selectbox(
-            "Sesso:", ["Maschio", "Femmina", "Non specificato"], index=2)
-        #email = cols[3].text_input("Età anagrafica:")
-
-        cols2 = st.columns((4))
-        #infrastruttura fisica tecnologica
-        date = cols2[0].date_input("Quando è stato in ospedale:")
-        infras = cols2[1].slider("Qualità della struttura ospedaliera :", 1, 100, 1)
-        proces = cols2[2].slider("Qualità dei processi clinici amministrativi :", 1, 100, 1)
-        sicurezza = cols2[3].slider("Sicurezza ospedale :", 1, 100, 1)
-
-        cols3 = st.columns((1, 1, 1, 1))
-        #risorse umane e accoglienza
-        qualita = cols3[0].slider("Qualità del personale :", 1, 100, 1)
-        pulizia = cols3[1].slider("Pulizia degli ambienti :", 1, 100, 1)
-        empatia = cols3[2].slider("Grado di empatia personale :", 1, 100, 1)
-        info_terapeutiche = cols3[3].slider("Chiarezze delle informazioni terapeutiche :", 1, 100, 1)
-        submitted = st.form_submit_button(label="Submit")
-        if submitted==True:
-            datetime_object = datetime.datetime.now()
-            st.success("Successfully")
-            add_row_to_gsheet(
-            df, [[author, eta, sesso,
-                  str(date), infras, proces, sicurezza,
-                  qualita, pulizia, empatia, info_terapeutiche, str(datetime_object)]])
-            st.balloons()
+    st.sidebar.title("Support")
+    st.sidebar.info(
+        """
+        Per eventuali problemi nell'utilizzo app rivolgersi a: DIG-CoE Pwc Italy
+        """
+    )
+    st.sidebar.image(image1, width=70)
