8031ea06ef5e3a5ef29d48ee657610ce1b835c4e
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-07 14:20:57 +0100

    Update 📊 Sezione Personale Sanitario.py

--- pages/📊 Sezione Personale Sanitario.py
+++ pages/📊 Sezione Personale Sanitario.py
@@ -4,65 +4,66 @@ import streamlit as st  # pip install streamlit
 import altair as alt
 #from Patient_Form import df, get_data
 
-st.title("Sezione Personale Sanitario")
+def dashboard_patient_satisf():
+    st.title("Sezione Personale Sanitario")
 
-# --- USER AUTHENTICATION ---
-def check_password():
-    """Returns `True` if the user had a correct password."""
+    # --- USER AUTHENTICATION ---
+    def check_password():
+        """Returns `True` if the user had a correct password."""
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if (
-            st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+        def password_entered():
+            """Checks whether a password entered by the user is correct."""
+            if (
+                st.session_state["username"] in st.secrets["passwords"]
+                and st.session_state["password"]
+                == st.secrets["passwords"][st.session_state["username"]]
+            ):
+                st.session_state["password_correct"] = True
+                del st.session_state["password"]  # don't store username + password
+                del st.session_state["username"]
+            else:
+                st.session_state["password_correct"] = False
+
+        if "password_correct" not in st.session_state:
+            # First run, show inputs for username + password.
+            st.text_input("Username", on_change=password_entered, key="username")
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+            st.write("Click enter to log")
+            return False
+        elif not st.session_state["password_correct"]:
+            # Password not correct, show input + error.
+            st.text_input("Username", on_change=password_entered, key="username")
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+            st.write("Click enter to log")
+            st.error("😕 User not known or password incorrect")
+            return False
         else:
-            st.session_state["password_correct"] = False
+            # Password correct
+            return True
 
-    if "password_correct" not in st.session_state:
-        # First run, show inputs for username + password.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.write("Click enter to log")
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.write("Click enter to log")
-        st.error("😕 User not known or password incorrect")
-        return False
-    else:
-        # Password correct
-        return True
+    if check_password(): 
+        st.markdown("Sezione dedicata agli specialisti di medicina generale per visionare andamento Poliambulatorio")
+        #st.dataframe(get_data(df))
 
-if check_password(): 
-    st.markdown("Sezione dedicata agli specialisti di medicina generale per visionare andamento Poliambulatorio")
-    #st.dataframe(get_data(df))
-    
-    #df1=pd.DataFrame(df)
-   
-    #c = alt.Chart(df1).mark_circle().encode(
-    #        x='Sesso', y='Sicurezza',)
-    #st.altair_chart(c, use_container_width=True)
-        
-    col1, col2, col3 = st.columns(3)
+        #df1=pd.DataFrame(df)
 
-    with col1:
-        st.header("A cat")
-         
-    with col2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
+        #c = alt.Chart(df1).mark_circle().encode(
+        #        x='Sesso', y='Sicurezza',)
+        #st.altair_chart(c, use_container_width=True)
 
-    with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.header("A cat")
+
+        with col2:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/dog.jpg")
+
+        with col3:
+            st.header("An owl")
+            st.image("https://static.streamlit.io/examples/owl.jpg")
