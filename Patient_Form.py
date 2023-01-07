7ff40e5dfa3b0ccd76d9742369ec094d555d3bb9
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-07 14:44:18 +0100

    Update Patient_Form.py

--- Patient_Form.py
+++ Patient_Form.py
@@ -18,8 +18,8 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
 
-from pages.Report_problemi import bug_report
-from pages.Dashboard import dashboard_patient_satisf
+from utils.Report_problemi import bug_report
+from utils.Dashboard import dashboard_patient_satisf
 
 image = Image.open('images/Medical_Logo.png')
 img = Image.open('images/background.jpg')
