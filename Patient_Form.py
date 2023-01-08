ef636984557848a807c07fc2fdff1615e88a107e
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-08 11:43:59 +0100

    Update Patient_Form.py

--- Patient_Form.py
+++ Patient_Form.py
@@ -99,7 +99,7 @@ name, authentication_status, username = authenticator.login("Login - Web applica
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
-    background-image: url("https://img.freepik.com/free-photo/background_53876-32169.jpg?w=2000");
+    background-image: url("https://www.xmple.com/wallpaper/one-colour-plain-green-solid-color-single-1920x1080-c-e0f4e4-f-24.svg");
     background-size: 250%;
     background-position: top right;
     background-repeat: no-repeat;
