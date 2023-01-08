3a1ab28f9a7b912fcd82cb13bfe75a9a33022416
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-08 15:42:03 +0100

    Update Dashboard.py

--- utils/Dashboard.py
+++ utils/Dashboard.py
@@ -6,64 +6,21 @@ import altair as alt
 
 def dashboard_patient_satisf():
     st.title("Sezione Personale Sanitario")
+    st.markdown("Sezione dedicata agli specialisti di medicina generale per visionare andamento Poliambulatorio")
+    #st.dataframe(get_data(df))
 
-    # --- USER AUTHENTICATION ---
-    def check_password():
-        """Returns `True` if the user had a correct password."""
+    #df1=pd.DataFrame(df)
 
-        def password_entered():
-            """Checks whether a password entered by the user is correct."""
-            if (
-                st.session_state["username"] in st.secrets["passwords"]
-                and st.session_state["password"]
-                == st.secrets["passwords"][st.session_state["username"]]
-            ):
-                st.session_state["password_correct"] = True
-                del st.session_state["password"]  # don't store username + password
-                del st.session_state["username"]
-            else:
-                st.session_state["password_correct"] = False
+    #c = alt.Chart(df1).mark_circle().encode(
+    #        x='Sesso', y='Sicurezza',)
+    #st.altair_chart(c, use_container_width=True)
 
-        if "password_correct" not in st.session_state:
-            # First run, show inputs for username + password.
-            st.text_input("Username", on_change=password_entered, key="username")
-            st.text_input(
-                "Password", type="password", on_change=password_entered, key="password"
-            )
-            st.write("Click enter to log")
-            return False
-        elif not st.session_state["password_correct"]:
-            # Password not correct, show input + error.
-            st.text_input("Username", on_change=password_entered, key="username")
-            st.text_input(
-                "Password", type="password", on_change=password_entered, key="password"
-            )
-            st.write("Click enter to log")
-            st.error("😕 User not known or password incorrect")
-            return False
-        else:
-            # Password correct
-            return True
+    col1, col2 = st.columns(2)
 
-    if check_password(): 
-        st.markdown("Sezione dedicata agli specialisti di medicina generale per visionare andamento Poliambulatorio")
-        #st.dataframe(get_data(df))
+    with col1:
+        st.header("Patient satisfaction")
+        st.image("https://www.datapine.com/images/patient-satisfaction-dashboard.png")
 
-        #df1=pd.DataFrame(df)
-
-        #c = alt.Chart(df1).mark_circle().encode(
-        #        x='Sesso', y='Sicurezza',)
-        #st.altair_chart(c, use_container_width=True)
-
-        col1, col2, col3 = st.columns(3)
-
-        with col1:
-            st.header("A cat")
-
-        with col2:
-            st.header("A dog")
-            st.image("https://static.streamlit.io/examples/dog.jpg")
-
-        with col3:
-            st.header("An owl")
-            st.image("https://static.streamlit.io/examples/owl.jpg")
+    with col2:
+        st.header("Health data")
+        st.image("https://www.datapine.com/images/hospital-kpi-dashboard.png")
