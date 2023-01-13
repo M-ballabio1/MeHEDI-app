ed16e362d39385ef74e2fdb547a3d2b53b890c47
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-13 20:53:29 +0100

    Update Dashboard_Operations.py

--- utils/Dashboard_Operations.py
+++ utils/Dashboard_Operations.py
@@ -13,59 +13,58 @@ SHEET_NAME = "Database_Operations"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
+def dashboard_operations():
+    @st.experimental_singleton()
+    def connect_to_gsheet():
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[SCOPE],
+        )
 
-    # Create a new Http() object for every request
-    def build_request(http, *args, **kwargs):
-        new_http = google_auth_httplib2.AuthorizedHttp(
+        # Create a new Http() object for every request
+        def build_request(http, *args, **kwargs):
+            new_http = google_auth_httplib2.AuthorizedHttp(
+                credentials, http=httplib2.Http()
+            )
+            return HttpRequest(new_http, *args, **kwargs)
+
+        authorized_http = google_auth_httplib2.AuthorizedHttp(
             credentials, http=httplib2.Http()
         )
-        return HttpRequest(new_http, *args, **kwargs)
+        service = build(
+            "sheets",
+            "v4",
+            requestBuilder=build_request,
+            http=authorized_http,
+        )
+        gsheet_connector = service.spreadsheets()
+        return gsheet_connector
 
-    authorized_http = google_auth_httplib2.AuthorizedHttp(
-        credentials, http=httplib2.Http()
-    )
-    service = build(
-        "sheets",
-        "v4",
-        requestBuilder=build_request,
-        http=authorized_http,
-    )
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
+    def get_data(gsheet_connector) -> pd.DataFrame:
+        values = (
+            gsheet_connector.values()
+            .get(
+                spreadsheetId=SPREADSHEET_ID,
+                range=f"{SHEET_NAME}!A:E",
+            )
+            .execute()
+        )
 
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
+        df = pd.DataFrame(values["values"])
+        df.columns = df.iloc[0]
+        df = df[1:]
+        return df
+
+    def add_row_to_gsheet(gsheet_connector, row) -> None:
+        gsheet_connector.values().append(
             spreadsheetId=SPREADSHEET_ID,
             range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
+            body=dict(values=row),
+            valueInputOption="USER_ENTERED",
+        ).execute()
 
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-def add_row_to_gsheet(gsheet_connector, row) -> None:
-    gsheet_connector.values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=f"{SHEET_NAME}!A:E",
-        body=dict(values=row),
-        valueInputOption="USER_ENTERED",
-    ).execute()
-
-
-
-def dashboard_operations():
+    
     st.title("Dashboard MedTech Operations")
     df = connect_to_gsheet()
     
