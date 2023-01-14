c515a52da7dc8392c10817584c5f2c2d0cd405c1
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-14 16:16:43 +0100

    Update Dashboard_Economics.py

--- utils/Dashboard_Economics.py
+++ utils/Dashboard_Economics.py
@@ -9,55 +9,6 @@ from googleapiclient.http import HttpRequest
 
 #from Patient_Form import form_pazienti
 
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1OBEMIUloci4WV80D-yLhhoLMVQymy-TYlh7jwGXmND8"
-SHEET_NAME = "Database_Operations"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
-
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
-
-    # Create a new Http() object for every request
-    def build_request(http, *args, **kwargs):
-        new_http = google_auth_httplib2.AuthorizedHttp(
-            credentials, http=httplib2.Http()
-        )
-        return HttpRequest(new_http, *args, **kwargs)
-
-    authorized_http = google_auth_httplib2.AuthorizedHttp(
-        credentials, http=httplib2.Http()
-    )
-    service = build(
-        "sheets",
-        "v4",
-        requestBuilder=build_request,
-        http=authorized_http,
-    )
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
-
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
-
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-df_operations=connect_to_gsheet()
-
 def dashboard_economics():
     
     st.title("Dashboard MedTech Economics")
@@ -68,6 +19,9 @@ def dashboard_economics():
     
     st.markdown("""**Questa sezione mostra i risultati dell'analisi utilizzando i dati delle operations di MeHedi""")
     
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        
     g1, g2 = st.columns(2)
     g1.metric(label = "Fatturato mensile", value = ("505.000€"), delta = ("20.000€"))
     g2.metric(label = "Costi menisili",
