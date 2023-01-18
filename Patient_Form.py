3ebf8c180bb13aca2c58b9b08376fd7a011bb93d
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-19 00:16:11 +0100

    Update Patient_Form.py

--- Patient_Form.py
+++ Patient_Form.py
@@ -89,120 +89,83 @@ st.set_page_config(page_title="MeHEDI", page_icon="📌", layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# --- USER AUTHENTICATION ---
 
-users = db.fetch_all_users()
-
-usernames = [user["key"] for user in users]
-names = [user["name"] for user in users]
-hashed_passwords = [user["password"] for user in users]
-
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "sales_dashboard", "abcdef", cookie_expiry_days=90)
-name, authentication_status, username = authenticator.login("Login - Web application MEHEDI Patient's satisfaction", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.write('<base target="_blank">', unsafe_allow_html=True)
-    prev_time = [time.time()]
-    a, b = st.columns([1, 1])
-    with a:
-        st.image(image, width=400)      
-    with b:
-        st.info(
-        """
-        Questa è una webapp creata da che consente di valutare la Patient Satisfaction in un'azienda sanitaria di medie dimensioni.
-        
-        ACCESSO PAZIENTE - username: guest | password: paz123
-        
-        ACCESSO MANAGEMENT - username: mballabio | password: mat123
-        """
-    ) 
-
-if authentication_status:
-    #placeholder.empty()
-
-    # ---- SIDEBAR ----
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title(f"Welcome {name}")
     
-    def form_pazienti():
-        t1, t2 = st.columns((1, 0.15)) 
+def form_pazienti():
+    t1, t2 = st.columns((1, 0.15)) 
 
-        t1.title("MedTech Hospital Experience Dashboard - MedMIB Hospital")
-        t1.markdown("### This section shows some information about MeHEDI - Tool of Patient Satisfaction")
-        t2.image(image, width = 250)
+    t1.title("MedTech Hospital Experience Dashboard - MedMIB Hospital")
+    t1.markdown("### This section shows some information about MeHEDI - Tool of Patient Satisfaction")
+    t2.image(image, width = 250)
 
-        #append pages
-        #st.sidebar.success("Select a page above.")
+    #append pages
+    #st.sidebar.success("Select a page above.")
 
-        # connect and append data
-        df = connect_to_gsheet()
-        #expander = st.expander("See all records")
-        #with expander:
-        #    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-        #    st.dataframe(get_data(df))
+    # connect and append data
+    df = connect_to_gsheet()
+    #expander = st.expander("See all records")
+    #with expander:
+    #    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+    #    st.dataframe(get_data(df))
 
-        with st.expander("ℹ️ Istruzioni generali", expanded=False):
-            st.markdown(
-                """
-                ## Informazioni Tool MeHEDI
-                L'obiettivo di questo tool è quello di fornire uno strumento per una misurazione
-                della soddsifazione dei pazienti
-                """
-            )
-            st.markdown("")
-            st.markdown(
-                """
-                ## Perchè misurare Patients Satisfaction?
-                L'efficacia di un'iniziativa di business non è data solo dalla misurazione degli outcome (es. revenue),
-                1. Miglioramento percepito della Customer Experience dei prodotti e servizi creati e lanciati rispetto al passato
-                2. Miglioramento dell'Employee Experience portata dal nuovo framework operativo
-                3. Miglioramento della performance di business (Outcome) - revenue, costi ecc
-                """
-            )
-            st.markdown("")
-            #st.image(image)
-        st.write("")
+    with st.expander("ℹ️ Istruzioni generali", expanded=False):
+        st.markdown(
+            """
+            ## Informazioni Tool MeHEDI
+            L'obiettivo di questo tool è quello di fornire uno strumento per una misurazione
+            della soddsifazione dei pazienti
+            """
+        )
+        st.markdown("")
+        st.markdown(
+            """
+            ## Perchè misurare Patients Satisfaction?
+            L'efficacia di un'iniziativa di business non è data solo dalla misurazione degli outcome (es. revenue),
+            1. Miglioramento percepito della Customer Experience dei prodotti e servizi creati e lanciati rispetto al passato
+            2. Miglioramento dell'Employee Experience portata dal nuovo framework operativo
+            3. Miglioramento della performance di business (Outcome) - revenue, costi ecc
+            """
+        )
+        st.markdown("")
+        #st.image(image)
+    st.write("")
 
 
-        # creazione webform
+    # creazione webform
 
-        form = st.form(key="annotation", clear_on_submit = True,)
-        with form:
-            cols = st.columns((1, 1, 1))
+    form = st.form(key="annotation", clear_on_submit = True,)
+    with form:
+        cols = st.columns((1, 1, 1))
 
-            #info paziente
-            author = cols[0].text_input("Nome del paziente:")
-            eta = cols[1].text_input("Età anagrafica:")
-            sesso = cols[2].selectbox(
-                "Sesso:", ["Maschio", "Femmina", "Non specificato"], index=2)
-            #email = cols[3].text_input("Età anagrafica:")
+        #info paziente
+        author = cols[0].text_input("Nome del paziente:")
+        eta = cols[1].text_input("Età anagrafica:")
+        sesso = cols[2].selectbox(
+            "Sesso:", ["Maschio", "Femmina", "Non specificato"], index=2)
+        #email = cols[3].text_input("Età anagrafica:")
 
-            cols2 = st.columns((4))
-            #infrastruttura fisica tecnologica
-            date = cols2[0].date_input("Quando è stato in ospedale:")
-            infras = cols2[1].slider("Qualità della struttura ospedaliera :", 1, 100, 1)
-            proces = cols2[2].slider("Qualità dei processi clinici amministrativi :", 1, 100, 1)
-            sicurezza = cols2[3].slider("Sicurezza ospedale :", 1, 100, 1)
+        cols2 = st.columns((4))
+        #infrastruttura fisica tecnologica
+        date = cols2[0].date_input("Quando è stato in ospedale:")
+        infras = cols2[1].slider("Qualità della struttura ospedaliera :", 1, 100, 1)
+        proces = cols2[2].slider("Qualità dei processi clinici amministrativi :", 1, 100, 1)
+        sicurezza = cols2[3].slider("Sicurezza ospedale :", 1, 100, 1)
 
-            cols3 = st.columns((1, 1, 1, 1))
-            #risorse umane e accoglienza
-            qualita = cols3[0].slider("Qualità del personale :", 1, 100, 1)
-            pulizia = cols3[1].slider("Pulizia degli ambienti :", 1, 100, 1)
-            empatia = cols3[2].slider("Grado di empatia personale :", 1, 100, 1)
-            info_terapeutiche = cols3[3].slider("Chiarezze delle informazioni terapeutiche :", 1, 100, 1)
-            submitted = st.form_submit_button(label="Submit")
-            if submitted==True:
-                datetime_object = datetime.datetime.now()
-                st.success("Successfully")
-                add_row_to_gsheet(
-                df, [[author, eta, sesso,
-                      str(date), infras, proces, sicurezza,
-                      qualita, pulizia, empatia, info_terapeutiche, str(datetime_object)]])
-                st.balloons()
+        cols3 = st.columns((1, 1, 1, 1))
+        #risorse umane e accoglienza
+        qualita = cols3[0].slider("Qualità del personale :", 1, 100, 1)
+        pulizia = cols3[1].slider("Pulizia degli ambienti :", 1, 100, 1)
+        empatia = cols3[2].slider("Grado di empatia personale :", 1, 100, 1)
+        info_terapeutiche = cols3[3].slider("Chiarezze delle informazioni terapeutiche :", 1, 100, 1)
+        submitted = st.form_submit_button(label="Submit")
+        if submitted==True:
+            datetime_object = datetime.datetime.now()
+            st.success("Successfully")
+            add_row_to_gsheet(
+            df, [[author, eta, sesso,
+                  str(date), infras, proces, sicurezza,
+                  qualita, pulizia, empatia, info_terapeutiche, str(datetime_object)]])
+            st.balloons()
                 
     if name=="Matteo Ballabio" or name=="Federico Facoetti" or name=="Luca Cappellini":
         page_names_to_funcs = {
