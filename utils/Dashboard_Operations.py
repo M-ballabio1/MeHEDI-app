08fc38a628c5383a2b6843dd3d171a883da40cdb
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-01-18 23:57:23 +0100

    Update Dashboard_Operations.py

--- utils/Dashboard_Operations.py
+++ utils/Dashboard_Operations.py
@@ -5,66 +5,20 @@ import datetime
 import streamlit as st
 from htbuilder import div, big, h2, styles
 from htbuilder.units import rem
-from openpyxl import Workbook
-from io import BytesIO
+import gspread as gs
 
 def dashboard_operations():
    SCOPE = "https://www.googleapis.com/auth/spreadsheets"
    SPREADSHEET_ID = "1OBEMIUloci4WV80D-yLhhoLMVQymy-TYlh7jwGXmND8"
    SHEET_NAME = "Database_Operations"
    GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
-
-   @st.experimental_singleton()
-   def connect_to_gsheet():
-       # Create a connection object.
-       credentials = service_account.Credentials.from_service_account_info(
-           st.secrets["gcp_service_account"],
-           scopes=[SCOPE],
-       )
-
-       # Create a new Http() object for every request
-       def build_request(http, *args, **kwargs):
-           new_http = google_auth_httplib2.AuthorizedHttp(
-               credentials, http=httplib2.Http()
-           )
-           return HttpRequest(new_http, *args, **kwargs)
-
-       authorized_http = google_auth_httplib2.AuthorizedHttp(
-           credentials, http=httplib2.Http()
-       )
-       service = build(
-           "sheets",
-           "v4",
-           requestBuilder=build_request,
-           http=authorized_http,
-       )
-       gsheet_connector = service.spreadsheets()
-       return gsheet_connector
-
-
-   def get_data(gsheet_connector) -> pd.DataFrame:
-       values = (
-           gsheet_connector.values()
-           .get(
-               spreadsheetId=SPREADSHEET_ID,
-               range=f"{SHEET_NAME}!A:E",
-           )
-           .execute()
-       )
-
-       df = pd.DataFrame(values["values"])
-       df.columns = df.iloc[0]
-       df = df[1:]
-       return df
-
-
-   def add_row_to_gsheet(gsheet_connector, row) -> None:
-       gsheet_connector.values().append(
-           spreadsheetId=SPREADSHEET_ID,
-           range=f"{SHEET_NAME}!A:E",
-           body=dict(values=row),
-           valueInputOption="USER_ENTERED",
-       ).execute()
+   
+   sh = gc.open_by_url(GSHEET_URL)
+   ws = sh.worksheet('Database_Operations')
+   
+   df = pd.DataFrame(ws.get_all_records())
+   df.head()
+   st.dataframe(df)
    
    def display_dial(title, value, color):
      st.markdown(
