5e665cd1637e34b3e02baa45b7c0405ca027a9a7
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-03-18 20:43:15 +0100

    5kpi + wordcloud

--- utils/Dashboard.py
+++ utils/Dashboard.py
@@ -1,4 +1,4 @@
-mport pandas as pd  # pip install pandas openpyxl
+import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 #import altair as alt
