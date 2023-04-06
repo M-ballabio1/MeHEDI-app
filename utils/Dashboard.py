cf47774f61aa2a74ded2bb107f48a447cbf84bc6
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-04-06 09:41:34 +0200

    Update Dashboard.py

--- utils/Dashboard.py
+++ utils/Dashboard.py
@@ -378,6 +378,12 @@ def dashboard_patient_satisf():
         st.text("")
     with col3:
         st.header("Dataframe Key-Words")
+        st.markdown("""
+        <div align=right><small>
+        Page views interaction: <img src="https://www.cutercounter.com/hits.php?id=hxndpfn&nd=6&style=52" border="0" alt="hit counter"><br>
+        GitHub <a href="https://github.com/M-ballabio1/MeHEDI-app"><img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/M-ballabio1/MeHEDI-app?style=social"></a>
+        </small></div>
+        """, unsafe_allow_html=True)
     
 
         def word_count(str):
