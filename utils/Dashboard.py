a32ae584b155537398ccf5b47a7288f5197901fd
Matteo <78934727+M-ballabio1@users.noreply.github.com>
2023-04-23 16:02:55 +0200

    Update Dashboard.py

--- utils/Dashboard.py
+++ utils/Dashboard.py
@@ -533,16 +533,17 @@ def dashboard_patient_satisf():
     x_range = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
     
     a,b,c=st.columns([1,1,1])
+    st.header("OLS Regression")
     with a:
-        st.header("OLS Regression - PSI e Soddisfazione Tempo d'attesa Risultati")
+        st.subheader("PSI e Soddisfazione Tempo d'attesa Risultati")
         fig = px.scatter(df5, x='Soddisf_Tempo_Attesa_Risult', y='PX', opacity=0.65, trendline="ols", trendline_color_override="red")
         st.plotly_chart(fig, use_container_width=True)
     with b:
-        st.header("OLS Regression - PSI e Soddisfazione Pulizia")
+        st.subheader("PSI e Soddisfazione Pulizia")
         fig = px.scatter(df5, x='Soddisf_Pulizia_Reparto', y='PX', opacity=0.65, trendline="ols", trendline_color_override="red")
         st.plotly_chart(fig, use_container_width=True)
     with c:
-        st.header("OLS Regression - PSI e Soddisfazione Privacy")
+        st.subheader("PSI e Soddisfazione Privacy")
         fig = px.scatter(df5, x='Soddisf_Privacy', y='PX', opacity=0.65, trendline="ols", trendline_color_override="red")
         st.plotly_chart(fig, use_container_width=True)
 
